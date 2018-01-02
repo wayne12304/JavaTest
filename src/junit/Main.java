80a1c7a45b9ec5b08321a5f608278410a4404196
MSI\Wayne <wayne12304@gmail.com>
2018-01-02 22:50:49 +0800

    123

---
+++ src/junit/Main.java
@@ -0,0 +1,16 @@
+package junit;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sIn = new Scanner(System.in);
+		do { 
+			int score = sIn.nextInt();
+			System.out.println(Grade.letterGrade(score));
+		} while(true);
+	}
+
+}
